7e69bce338fc5f33e5cfea5c633bccaf0dc3fe06
MASIOUB <93975488+MASIOUB@users.noreply.github.com>
2022-04-06 10:19:34 +0000

    add file of presentation

--- app/views/home.php
+++ app/views/home.php
@@ -30,18 +30,6 @@ welcom to home page !!
 <br>
 <br>
 <?php
-// function generateRandomString($length = 10) {
-//     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-//     $charactersLength = strlen($characters);
-//     $randomString = '';
-//     for ($i = 0; $i < $length; $i++) {
-//         $randomString .= $characters[rand(0, $charactersLength - 1)];
-//     }
-//     return $randomString;
-// }
 
-// $s = generateRandomString();
-
-// echo generateRandomString();
 
 ?>
